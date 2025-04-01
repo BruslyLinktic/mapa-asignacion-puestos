d9840aa3fc6b256a80547accc641e7f00ba1fc3f
BruslyLinktic <brandow.leon@linktic.com>
2025-04-01 15:40:25 -0500

    Añadir página del editor

---
+++ src/pages/EditorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapDataEditor from '../components/MapDataEditor';
+
+const EditorPage = () => {
+  return (
+    <div className="container">
+      <h1>Editor del Mapa de Puestos</h1>
+      <MapDataEditor />
+    </div>
+  );
+};
+
+export default EditorPage;
